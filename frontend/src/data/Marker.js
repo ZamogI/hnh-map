8e8b9e15372e4d89e10f5423b980747dd9ec90f5
ZamogI <zamog95@gmail.com>
2023-11-03 11:38:59 +0300

    Added icons for caveout, thingwall, toad quest giver, totems

--- frontend/src/data/Marker.js
+++ frontend/src/data/Marker.js
@@ -36,10 +36,13 @@ export class Marker {
             
             if(this.image == "gfx/terobjs/mm/custom") {
                 icon = new ImageIcon({iconUrl: 'gfx/terobjs/mm/custom.png', iconSize: [21, 23], iconAnchor: [11, 21], popupAnchor: [1, 3], tooltipAnchor: [1, 3]})
-            } else {
+            } else if(this.image == "gfx/terobjs/mm/cavein" || this.image == "gfx/terobjs/mm/caveout"){
+				icon = new ImageIcon({iconUrl: `${this.image}.png`, iconSize: [16, 16]});
+			}
+			else {
                 icon = new ImageIcon({iconUrl: `${this.image}.png`, iconSize: [32, 32]});
             }
-            
+			            
             let position = mapview.map.unproject([this.position.x, this.position.y], HnHMaxZoom);
             this.marker = L.marker(position, {icon: icon, title: this.name});
             this.marker.addTo(mapview.markerLayer);
